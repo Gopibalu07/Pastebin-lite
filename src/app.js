19b9d8b9ee077e8f7c821ab3bdda464b8aa2ea96
Gopinath <gopinathbalu07@gmail.com>
2026-01-29 10:12:07 +0530

    add root route

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Pastebin Lite API is running");
+});
+
 app.use("/api", healthRoutes);
 app.use("/api/pastes", pasteRoutes);
 app.get("/p/:id", controller.renderPaste);
