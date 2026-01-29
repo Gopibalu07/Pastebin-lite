d7001958f3cbe60c27a51be9ef0c0e21ac704a03
Gopinath <gopinathbalu07@gmail.com>
2026-01-29 10:24:28 +0530

    add root route

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 1,
+  idleTimeoutMillis: 5000,
+  connectionTimeoutMillis: 2000,
 });
 
 module.exports = pool;
